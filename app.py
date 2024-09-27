4519d76ad0d6954cd58da00ce032561da422732a
Seu Nome <riquelmyvasconcelosdev@gmail.com>
2024-09-26 23:08:26 -0300

    aula de crud, adicionando o get e o post

--- app.py
+++ app.py
@@ -1,21 +1,40 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.task import Task
 
 # __name__ = "__main__"
 app = Flask(__name__)
 
-# criando uma rota
-@app.route("/")
+# CRUD
+# Create, Read, Update, Delete
+# Tabela: Tarefa
 
-# oque será executado quando alguém acessar a rota
-def hello_world():
-  return "Hello, World!"
+tasks = []
+task_id_control = 1
 
-@app.route("/about")
-def about():
-  return "About page"
+@app.route('/tasks', methods=['POST'])
+def create_task():
+  global task_id_control #sempre que for usar uma variavel que for fazer intereção dentro desse metodo, use o GLOBAl, pq ele consegue fazer referencia com aquilo que está fora do medotodo
+  data = request.get_json()
+  new_task = Task(id=task_id_control, title=data.get("title"), description=data.get("description", ""))
+  task_id_control += 1
+  tasks.append(new_task) 
+  print(tasks)
+  return jsonify({"mensagem": "Nova tarefa criada com sucesso!"})
+  
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+  tasks_list = [task.to_dict() for task in tasks]
+  
+
+  output = {
+                "tasks": tasks_list,
+                "total_tasks": 0
+            }
+
+  return jsonify(output)
+
 
 # executando a rota
-if __name__ == "__main__": # isso é para garantir, que só quando a gente execute ele de forma manual, vamos subir o servidor dessa forma
+if __name__ == "__main__": #isso é para garantir, que só quando a gente execute ele de forma manual, vamos subir o servidor dessa forma
   app.run(debug=True)
 
-teste,1 2 3
