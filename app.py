1113bed6008918d77869235c80a86386423bbfa5
Seu Nome <riquelmyvasconcelosdev@gmail.com>
2024-12-18 21:36:04 -0300

    Final do projeto

--- app.py
+++ app.py
@@ -13,36 +13,76 @@ task_id_control = 1
 
 @app.route('/tasks', methods=['POST'])
 def create_task():
-  global task_id_control #sempre que for usar uma variavel que for fazer intereção dentro desse metodo, use o GLOBAl, pq ele consegue fazer referencia com aquilo que está fora do medotodo
-  data = request.get_json()
-  new_task = Task(id=task_id_control, title=data.get("title"), description=data.get("description", ""))
-  task_id_control += 1
-  tasks.append(new_task) 
-  print(tasks)
-  return jsonify({"mensagem": "Nova tarefa criada com sucesso!"})
-  
+    global task_id_control # sempre que for usar uma variável que for fazer interação dentro desse método, use o GLOBAL, porque ele consegue fazer referência com aquilo que está fora do método
+    data = request.get_json()
+    new_task = Task(id=task_id_control, title=data.get("title"), description=data.get("description", ""))
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"mensagem": "Nova tarefa criada com sucesso!"})
+
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
-  tasks_list = [task.to_dict() for task in tasks]
-  
-
-  output = {
-                "tasks": tasks_list,
-                "total_tasks": len(tasks_list)
-            }
-
-  return jsonify(output)
-
-@app.route('/tasks/<int:id>', methods=['GET'])
-def get_task(id):
-  for t in tasks:
-    if t.id == id:
-      return jsonify(t.to_dict())
+    tasks_list = [task.to_dict() for task in tasks]
     
-  return jsonify({"mensagem": "Não foi possível encontrar a atividade"}), 404
+    output = {
+        "tasks": tasks_list,
+        "total_tasks": len(tasks_list)
+    }
 
+    return jsonify(output)
+
+@app.route('/tasks/<int:id>', methods=['GET']) # R do CRUD
+def get_task(id):
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+            
+                        
+    return jsonify({"mensagem": "Não foi possível encontrar a atividade"}), 404
+
+"""# estudar a documentação https://flask.palletsprojects.com/en/3.0.x/quickstart/#routing
+@app.route('/user/<int:user_id>') # consigo receber os indicadores em ( string, int, float, path, uuid.)
+def show_user(user_id):
+    print(user_id)
+    print(type(user_id))
+    return "%s" % user_id"""
+
+@app.route('/tasks/<int:id>', methods=["PUT"])    
+def update_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+
+    print(task)        
+    if task == None:
+        return jsonify({"mensagem": "Não foi possível encontrar a atividade"}), 404
+    
+    data = request.get_json()
+    task.title = data['title']
+    task.description = data['description']
+    task.completed = data['completed']
+    print(task)
+    return jsonify({"message": "Tarefa atualizada com sucesso"})
+
+@app.route('/tasks/<int:id>', methods=["DELETE"])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+
+    print(task)        
+    if task == None:
+        return jsonify({"mensagem": "Não foi possível encontrar a atividade"}), 404
+
+    tasks.remove(task)
+    return jsonify({"message": "Tarefa deletada com sucesso"})
 
 # executando a rota
-if __name__ == "__main__": #isso é para garantir, que só quando a gente execute ele de forma manual, vamos subir o servidor dessa forma
-  app.run(debug=True)
+if __name__ == "__main__": # isso é para garantir, que só quando a gente execute ele de forma manual, vamos subir o servidor dessa forma
+    app.run(debug=True)
 
